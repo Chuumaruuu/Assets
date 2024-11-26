e652c121fb33dd8492f6e3f196c3554278316dab
YaBoiEley <axlisatschool@gmail.com>
2024-11-26 22:21:50 +0800

    added tp stuff hope it works

--- Scripts/StageOneScripts/PortalScript.cs
+++ Scripts/StageOneScripts/PortalScript.cs
@@ -5,32 +5,10 @@ using UnityEngine;
 public class PortalScript : MonoBehaviour
 {
     public Transform teleportTarget;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        teleportTarget = GameObject.Find("TeleportStageOneHere").transform;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject player;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            other.transform.position = teleportTarget.position;
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player") && gameObject.CompareTag("Portal1"))
-        {
-            collision.gameObject.transform.position = teleportTarget.position;
-        }
+        player.transform.position = teleportTarget.transform.position;
     }
 }
