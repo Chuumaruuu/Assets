e2f8be203dc07e2a02a5ef77d78fb3b91f1e593f
YaBoiEley <axlisatschool@gmail.com>
2024-11-27 12:09:32 +0800

    fixed my prefab droopy hand, added portal teleport condition, fixed target orientation on second island

--- Scripts/StageOneScripts/PortalScript.cs
+++ Scripts/StageOneScripts/PortalScript.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class PortalScript : MonoBehaviour
 {
     public Transform destination;
+    public GameObject coin; // Reference to the coin object
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !coin.activeInHierarchy) // Check if the coin is inactive
         {
             other.gameObject.SetActive(false);
             other.transform.position = destination.position;
